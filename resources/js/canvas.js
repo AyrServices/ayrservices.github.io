6560ba7f7822e2c8bf68fe8e6534d3c08c450c51
AyrServices <63121172+AyrServices@users.noreply.github.com>
2020-04-03 21:33:20 +0300

    Initial Push of Files
    Creating Base Files

---
+++ resources/js/canvas.js
@@ -0,0 +1,29 @@
+/*VANTA.NET({
+    el: "#vantajs",
+    mouseControls: true,
+    touchControls: true,
+    minHeight: 700.00,
+    minWidth: 500.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x3fbbff,
+    backgroundColor: 0x1b153c,
+    points: 8.00,
+    maxDistance: 24.00,
+    spacing: 18.00
+})*/
+VANTA.BIRDS({
+    el: "#vantajs",
+    mouseControls: true,
+    touchControls: true,
+    minHeight: 950.00,
+    minWidth: 2000.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    birdSize: 0.60,
+    wingSpan: 40.00,
+    speedLimit: 8.00,
+    separation: 40.00,
+    alignment: 30.00,
+    quantity: 4.00
+})
